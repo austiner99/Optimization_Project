0e099c42bab7bbd680d1ae8e43b5a411a495bf4e
Ammon Miller <aemill17@byu.edu>
2025-04-02 15:31:14 -0600

    ammon broke is rearranging obj v3

---
+++ RUNME.py
@@ -0,0 +1,30 @@
+'''
+The purpose of this code is...
+
+
+Authors:
+ - Austin Erickson "The Brain"
+ - Isaac Detiege "The Muscle"
+ - Ammon Miller "The Milkman" (Copilot generated, lol)
+
+Last updated: 3/27/2025 around 3pm
+
+Notes:
+- Roll the dice method (40% p1, 40% p2, 20% mutation)
+
+- This function calls on
+'''
+#---------------------------------
+# Import 
+#---------------------------------
+# import numpy as np
+import matplotlib.pyplot as plt
+from pathos.multiprocessing import ProcessingPool as Pool
+import heapq
+import parameters as P
+from string_paragraph import string, string_to_index_optimized
+import generate_func as gen_func
+
+
+print(string_to_index_optimized(string))
+
